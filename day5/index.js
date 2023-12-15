86903e9dc8182b13ad2e14480dbfe15d7687ce27
Josh Sikorski <josh.sikorski@gmail.com>
2023-12-15 12:05:41 -0600

    Solved day 5, part 2

--- day5/index.js
+++ day5/index.js
@@ -1,3 +1,4 @@
+const { Console } = require('node:console')
 const fs = require('node:fs/promises')
 
 async function solvePartOne ( filename) {
@@ -41,7 +42,6 @@ async function solvePartOne ( filename) {
             let currentArrayOfMaps = mappingData.get(currentSubject).arrayOfMaps
             // let currentSubjectMap = new Map(mappingData.get(currentSubject).mappedValues)
             // console.log(currentArrayOfMaps)
-            let priorValue = currentValue
             // let newValue = currentSubjectMap.get(Number(currentValue))
             let newValue = await findDestinationValue(currentValue, currentArrayOfMaps)
             if (newValue) currentValue = newValue
@@ -77,8 +77,8 @@ async function processMapLines ( mapSection, linesInSection) {
         let destinationRangeStart = Number(numbersInLine[0])
         let sourceRangeStart = Number(numbersInLine[1])
         let rangeLength = Number(numbersInLine[2])
-        let destinationRangeEnd = Number(destinationRangeStart) + Number(rangeLength)
-        let sourceRangeEnd = Number(sourceRangeStart) + Number(rangeLength)
+        let destinationRangeEnd = Number(destinationRangeStart) + Number(rangeLength) - 1
+        let sourceRangeEnd = Number(sourceRangeStart) + Number(rangeLength) - 1
         // console.log(`destinationRangeStart ${destinationRangeStart}, destinationRangeEnd ${destinationRangeEnd}, rangeLength ${rangeLength}`)
         // console.log(`sourceRangeStart ${sourceRangeStart}, sourceRangeEnd ${sourceRangeEnd}, rangeLength ${rangeLength}`)
         arrayOfMaps.push({
@@ -95,8 +95,42 @@ async function processMapLines ( mapSection, linesInSection) {
         sourceSubject,
         destinationSubject,
         arrayOfMaps
-    }
-    
+    } 
+}
+
+async function processMapLinesInReverse ( mapSection, linesInSection) {
+    let wordsInMapSection = mapSection.split(' ')[0].split('-')
+    let destinationSubject = wordsInMapSection[0]
+    let sourceSubject = wordsInMapSection[2]
+    let arrayOfMaps = []
+    for (line of linesInSection) {       
+        let numbersInLine = line.match(/\d+/g)
+        //  let destinationRangeStart = Number(numbersInLine[0])
+        // let sourceRangeStart = Number(numbersInLine[1])
+        // let rangeLength = Number(numbersInLine[2])
+        // let destinationRangeEnd = Number(destinationRangeStart) + Number(rangeLength)
+        // let sourceRangeEnd = Number(sourceRangeStart) + Number(rangeLength)
+        
+        
+        let sourceRangeStart = Number(numbersInLine[0])
+        let destinationRangeStart = Number(numbersInLine[1])
+        let rangeLength = Number(numbersInLine[2])
+        let sourceRangeEnd = Number(sourceRangeStart) + Number(rangeLength) - 1
+        let destinationRangeEnd = Number(destinationRangeStart) + Number(rangeLength) - 1
+        // console.log(`destinationRangeStart ${destinationRangeStart}, destinationRangeEnd ${destinationRangeEnd}, rangeLength ${rangeLength}`)
+        // console.log(`sourceRangeStart ${sourceRangeStart}, sourceRangeEnd ${sourceRangeEnd}, rangeLength ${rangeLength}`)
+        arrayOfMaps.push({
+            sourceRangeStart,
+            sourceRangeEnd,
+            destinationRangeStart,
+            destinationRangeEnd
+        })
+    }    
+    return {
+        sourceSubject,
+        destinationSubject,
+        arrayOfMaps
+    } 
 }
 
 async function solvePartTwo ( filename ) {
@@ -105,18 +139,88 @@ async function solvePartTwo ( filename ) {
     let lines = fileInput.trim().split('\n')
     file.close()
 
+    let seedList = []
+    let mappingData = new Map()
+
+    for ( let lineNumber = 0; lineNumber < lines.length; lineNumber++ ) {
+        // Build mapping ranges for seeds
+        if ( lineNumber === 0) {
+            let arrayOfMaps = []
+            seedList = lines[lineNumber].match(/\d+/g)
+            for (let seedRange = 0; seedRange < seedList.length; seedRange = seedRange + 2) {
+                arrayOfMaps.push({
+                    sourceRangeStart: Number(seedList[seedRange]),
+                    sourceRangeEnd: Number(seedList[seedRange]) + Number(seedList[seedRange + 1]) - 1
+                })
+            }
+            console.log(`arrayOfMaps:`)
+            console.log(arrayOfMaps)
+            mappingData.set('seed', {
+                arrayOfMaps: arrayOfMaps
+            })
+        }
+
+        if (lines[lineNumber].trim() === '') {
+            lineNumber++
+            let mapSection = lines[lineNumber].trim()
+            let linesInSection = []
+            lineNumber++
+            while (lineNumber < lines.length && lines[lineNumber].trim() !== '') {
+                linesInSection.push(lines[lineNumber])
+                lineNumber++
+            }
+            let processedData = await processMapLinesInReverse(mapSection, linesInSection)
+            mappingData.set(processedData.sourceSubject, {
+                destinationSubject: processedData.destinationSubject,
+                arrayOfMaps: processedData.arrayOfMaps
+            })
+            lineNumber--
+        }
+    }
+
+    let minLocation = 0
+    let seedExistsForLocation = false
+    while (!seedExistsForLocation) {
+        
+        if (minLocation % 500000 === 0 ) console.log(`checking location ${minLocation}`)
+        let currentSubject = 'location'
+        let currentValue = minLocation
+        while (currentSubject !== 'seed') {
+            let currentArrayOfMaps = mappingData.get(currentSubject).arrayOfMaps
+            // console.log(currentArrayOfMaps)
+            let newValue = await findDestinationValue(currentValue, currentArrayOfMaps)
+            if (newValue) currentValue = newValue
+            currentSubject = mappingData.get(currentSubject).destinationSubject
+        }
+        // Current subject is now seed.  Check if the seed is in the range
+        // console.log(`checking if seed ${currentValue} is in the range`)
+        let seedMap = mappingData.get('seed').arrayOfMaps
+        let seedExists = false
+        for (let i = 0; i < seedMap.length; i++) {
+            if ( Number(currentValue) >= Number(seedMap[i].sourceRangeStart) && Number(currentValue) <= Number(seedMap[i].sourceRangeEnd)) {
+                console.log(`${currentValue} is between ${seedMap[i].sourceRangeStart} and ${seedMap[i].sourceRangeEnd}`)
+                seedExists = true
+                break
+            }
+        }
+        
+        if (seedExists) {
+            seedExistsForLocation = true
+        } else {
+            minLocation++
+        }
+    }
     
+    return minLocation
 }
 
-async function buildSeedToSoilMap () {
+// solvePartOne('./input.txt')
+//     .then(answer => console.log('answer:', answer))
 
-}
-
-// solvePartOne('./tests/data/input.txt')
-solvePartOne('./input.txt')
+solvePartTwo('./input.txt')
+//solvePartTwo('./tests/data/input.txt')
     .then(answer => console.log('answer:', answer))
 
-// solvePartTwo('./input.txt')
-//     .then(sumOfGames => console.log('sumOfGames:', sumOfGames))
+// NOT 52210645 - too high
 
 module.exports = { solvePartOne, solvePartTwo }
--- day5/tests/index.test.js
+++ day5/tests/index.test.js
@@ -1,16 +1,21 @@
 const index = require('../index.js')
 
 test('result should be 35 with sample data', async () => {
-    let partOneAnswer = await index.solvePartOne('./tests/data/input.txt')
-    expect(partOneAnswer).toBe(35)
+    let answer = await index.solvePartOne('./tests/data/input.txt')
+    expect(answer).toBe(35)
 })
 
-test('result should be 340994526 with my data', async () => {
-    let partOneAnswer = await index.solvePartOne('./input.txt')
-    expect(partOneAnswer).toBe(340994526)
+// test('result should be 340994526 with my data', async () => {
+//     let answer = await index.solvePartOne('./input.txt')
+//     expect(answer).toBe(340994526)
+// })
+
+test('part 2 result should be 46 with sample data', async () => {
+    let answer = await index.solvePartTwo('./tests/data/input.txt')
+    expect(answer).toBe(46)
 })
 
-// test('part 2 result should be 63307 with my data', async () => {
-//     let partTwoAnswer = await index.solvePartTwo('./input.txt')
-//     expect(partTwoAnswer).toBe(63307)
+// test('part 2 result should be 52210644 with my data', async () => {
+//     let answer = await index.solvePartTwo('./input.txt')
+//     expect(answer).toBe(52210644)
 // })
