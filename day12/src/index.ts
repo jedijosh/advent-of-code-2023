74c6f3e2747fcab201103d02e72f6fc7b2b80310
Josh Sikorski <josh.sikorski@gmail.com>
2023-12-25 14:35:22 -0600

    WIP: Day 12, part 2.

--- day12/src/index.ts
+++ day12/src/index.ts
@@ -1,6 +1,6 @@
 import { parseFileIntoArrayOfLines } from './utils'
 
-const LOGGING = false
+const LOGGING = true
 
 export async function solvePartOne ( filename : string) {
     let possibleArrangements: number = 0
@@ -34,19 +34,19 @@ export async function solvePartOne ( filename : string) {
         for (let location of unknownLocations) {
             if (location.index !== undefined) positionOfUnknownLocations.push(location.index)
         }
-        if (LOGGING) console.log('positionOfUnknownLocations', positionOfUnknownLocations)        
+        // if (LOGGING) console.log('positionOfUnknownLocations', positionOfUnknownLocations)        
 
         let combinations: Array<Array<number>> = findCombinations(new Array<number>(), positionOfUnknownLocations, numberOfSpringsToFind)
-        if (LOGGING) console.log('combinations', combinations)
+        // if (LOGGING) console.log('combinations', combinations)
 
         for (let combinationNumber = 0; combinationNumber < combinations.length; combinationNumber++) {
             let stringToTest = conditionRecord.replace(/\?/g, '.')
-            if (LOGGING) console.log('stringToTest', stringToTest)
+            // if (LOGGING) console.log('stringToTest', stringToTest)
             for (let positionNumber of combinations[combinationNumber]) {
                 let stringForCombination = stringToTest.substring(0,positionNumber) + '#' + stringToTest.substring(positionNumber+1)
                 stringToTest = stringForCombination
             }
-            if (LOGGING) console.log('stringToTest', stringToTest)
+            // if (LOGGING) console.log('stringToTest', stringToTest)
             if ( stringToTest.match(regexToMatch)) possibleArrangements++
         }
         
@@ -92,19 +92,109 @@ export function findCombinations(inputArray: Array<number> = [], availableValues
 
 
 export async function solvePartTwo ( filename : string) {
-    let pathLength: number = 0
+    let possibleArrangements: number = 0
     let fileLines : String[] = await parseFileIntoArrayOfLines(filename)
-
     
-    return pathLength
+    for (let line of fileLines) {
+        let splitLine = line.split(' ')
+        // Condition record is the string containing the known springs and missing data.
+        let conditionRecord: string = splitLine[0] || ''
+        // Group order is an array containing the numbers at the end of the line.  Example: "1, 1, 3"
+        let groupOrder: string = splitLine[1] || ''
+        if (LOGGING) console.log('conditionRecord', conditionRecord)
+        if (LOGGING) console.log('groupOrder', groupOrder)
+
+        // See if we can strip off any of the string from the beginning or the end
+        // Look for one or more periods, one or more #, zero or more "." and then end of line
+        let matchesAtEndOfString = [...conditionRecord.matchAll(/(\.+\#+\.*)+$/g)]
+        for (let match of matchesAtEndOfString) {
+            console.log('match', match)
+            let newString: string = conditionRecord.substring(0, match.index)
+            console.log('length', match.length)
+            for (let i = 0; i < match.length; i++) {
+                console.log('popping')
+                groupOrder = groupOrder.split(',').pop() || ''
+            }
+            conditionRecord = newString
+            if (LOGGING) console.log('conditionRecord', conditionRecord)
+            if (LOGGING) console.log('groupOrder', groupOrder)
+        }
+
+        // Split the line based on "."s.  This will give the total number of sections to be solved
+        
+        let sectionsToBeSolved = conditionRecord.matchAll(/\.+/g)
+        console.log('sectionsToBeSolved', sectionsToBeSolved)
+        // // // If there were periods at the beginning, ignore from the count.
+        let numberOfSections: number = 0
+        for (let section of sectionsToBeSolved) {
+            numberOfSections++
+        }
+        for (let section of sectionsToBeSolved) {
+            console.log('section', section)
+        }
+
+        // If there are the same number of sections to be solved as numbers in the groupOrder
+        // there are periods between each section already so just loop through each section.
+        if (numberOfSections + 1 === groupOrder.length) {
+
+        }
+
+        return 0
+
+
+
+
+        // Calculate the total number of damaged springs for the line
+        // Also create a RegEx to check if the full filled in line matches the "groupOrder" numbers
+        // let totalNumberOfDamagedSprings = 0
+        // let regexString = "^\\.*" // might start with periods
+        // for (let number of groupOrder.split(',')) {
+        //     totalNumberOfDamagedSprings += Number(number)
+        //     regexString += "\\#{" + number + "}\\.+"
+        // }
+        // regexString = regexString.substring(0, regexString.length - 1) + '*$'
+        // let regexToMatch = new RegExp(regexString)
+
+        // let numberOfKnownDamagedSprings = 0
+        // // Find the location of each damaged spring within the condition record.
+        // // Only currently using to calculate how many we know about.
+        // let damagedSpringLocations = conditionRecord.match(/\#{1}/g) || []
+        // numberOfKnownDamagedSprings = damagedSpringLocations.length
+        // let numberOfSpringsToFind: number = totalNumberOfDamagedSprings - numberOfKnownDamagedSprings
+        // if (LOGGING) console.log(`Know of ${numberOfKnownDamagedSprings} and there are ${totalNumberOfDamagedSprings} total so ${numberOfSpringsToFind} to find`)
+
+        // // Find the positions of each unknown location in the full string
+        // let unknownLocations = conditionRecord.matchAll(/\?{1}/g) || []
+        // let positionOfUnknownLocations: Array<number> = []
+        // for (let location of unknownLocations) {
+        //     if (location.index !== undefined) positionOfUnknownLocations.push(location.index)
+        // }
+        // // if (LOGGING) console.log('positionOfUnknownLocations', positionOfUnknownLocations)        
+
+        // let combinations: Array<Array<number>> = findCombinations(new Array<number>(), positionOfUnknownLocations, numberOfSpringsToFind)
+        // // if (LOGGING) console.log('combinations', combinations)
+
+        // for (let combinationNumber = 0; combinationNumber < combinations.length; combinationNumber++) {
+        //     let stringToTest = conditionRecord.replace(/\?/g, '.')
+        //     // if (LOGGING) console.log('stringToTest', stringToTest)
+        //     for (let positionNumber of combinations[combinationNumber]) {
+        //         let stringForCombination = stringToTest.substring(0,positionNumber) + '#' + stringToTest.substring(positionNumber+1)
+        //         stringToTest = stringForCombination
+        //     }
+        //     // if (LOGGING) console.log('stringToTest', stringToTest)
+        //     if ( stringToTest.match(regexToMatch)) possibleArrangements++
+        // }
+    }
+
+    return possibleArrangements
 }
 
 
 
 // solvePartOne('/mnt/c/Users/joshs/code/advent-of-code-2023/day12/tests/data/input.txt')
-solvePartOne('/mnt/c/Users/joshs/code/advent-of-code-2023/day12/input.txt')
-    .then(answer => console.log('answer:', answer))
+// solvePartOne('/mnt/c/Users/joshs/code/advent-of-code-2023/day12/input.txt')
+    // .then(answer => console.log('answer:', answer))
 
-// solvePartTwo('/mnt/c/Users/joshs/code/advent-of-code-2023/day12/tests/data/input.txt')
+solvePartTwo('/mnt/c/Users/joshs/code/advent-of-code-2023/day12/tests/data/input.txt')
 // solvePartTwo('/mnt/c/Users/joshs/code/advent-of-code-2023/day12/input.txt')
-        // .then(answer => console.log('answer:', answer))
+        .then(answer => console.log('answer:', answer))
